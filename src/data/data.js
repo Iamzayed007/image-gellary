444f2697d5ff373346f885f84efc71add2d4f094
Iamzayed007 <72243831+Iamzayed007@users.noreply.github.com>
2023-11-01 19:38:22 +0600

    render image

---
+++ src/data/data.js
@@ -0,0 +1,61 @@
+import Image1 from "../assets/images/image-1.webp"
+import Image2 from "../assets/images/image-2.webp"
+import Image3 from "../assets/images/image-3.webp"
+import Image4 from "../assets/images/image-4.webp"
+import Image5 from "../assets/images/image-5.webp"
+import Image6 from "../assets/images/image-6.webp"
+import Image7 from "../assets/images/image-7.webp"
+import Image8 from "../assets/images/image-8.webp"
+import Image9 from "../assets/images/image-9.webp"
+import Image10 from "../assets/images/image-10.jpeg"
+import Image11 from "../assets/images/image-11.jpeg"
+
+
+export const imagesData = [
+    {
+      id:1,
+      url:Image1,
+    },
+    {
+      id:2,
+      url: Image2,
+    },
+    {
+      id:3,
+      url: Image3,
+    },
+    {
+      id:4,
+      url: Image4,
+    },
+    {
+      id:5,
+      url: Image5,
+    },
+    {
+      id:6,
+      url: Image6
+    },
+    {
+      id:7,
+      url: Image7
+    },
+    {
+      id:8,
+      url: Image8
+    },
+    {
+      id:9,
+      url: Image9
+    },
+    {
+      id:10,
+      url: Image10
+    },
+    {
+      id:11,
+      url: Image11
+    },
+  
+   
+  ];
