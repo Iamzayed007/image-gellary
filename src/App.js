36935f89361ed1daf0cd334e68b6766458787460
Iamzayed007 <72243831+Iamzayed007@users.noreply.github.com>
2023-11-01 18:06:32 +0600

    initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+
+import './App.css';
+import Gallery from './components/Gallery/Gallery';
+
+function App() {
+  return (
+    <div>
+<h1>Image</h1>
+<Gallery/>
+    </div>
+  );
+}
+
+export default App;
